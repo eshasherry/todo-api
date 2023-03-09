f36b37c8f424de945ec06fae532e44b67fe4eec0
eshasherry <esha.sherry@gmail.com>
2023-03-09 16:27:34 -0500

    Initial commit

---
+++ src/test/java/com/rest/webservice/todoapi/TodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.webservice.todoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
