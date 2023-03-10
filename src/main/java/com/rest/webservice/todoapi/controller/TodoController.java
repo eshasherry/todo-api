1f18602435fa88031fba194c1bbfcdab52fcab35
eshasherry <esha.sherry@gmail.com>
2023-03-09 21:52:50 -0500

    adding delete mapping

--- src/main/java/com/rest/webservice/todoapi/controller/TodoController.java
+++ src/main/java/com/rest/webservice/todoapi/controller/TodoController.java
@@ -3,6 +3,7 @@ package com.rest.webservice.todoapi.controller;
 import com.rest.webservice.todoapi.entity.Todo;
 import com.rest.webservice.todoapi.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +17,9 @@ public class TodoController {
     @GetMapping("/{username}/todos")
     public List<Todo> getTodos(@PathVariable String username){
         return todoRepository.findTodosByUsername(username);
+    }
+    @DeleteMapping("/{id}/todo")
+    public void deleteTodos(@PathVariable int id){
+        todoRepository.deleteById(id);
     }
 }
