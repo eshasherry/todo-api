5d5812d88e7d490f30549ea5a230f5d49b6f7c2e
eshasherry <esha.sherry@gmail.com>
2023-03-12 17:34:24 -0400

    updating the return value to ResponseEntity in controller

--- src/main/java/com/rest/webservice/todoapi/controller/TodoController.java
+++ src/main/java/com/rest/webservice/todoapi/controller/TodoController.java
@@ -4,6 +4,8 @@ import com.rest.webservice.todoapi.entity.Todo;
 import com.rest.webservice.todoapi.exception.TodoNotFoundException;
 import com.rest.webservice.todoapi.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,28 +15,28 @@ public class TodoController {
     @Autowired
     TodoRepository todoRepository;
     @GetMapping("users/{username}/todos")
-    public List<Todo> getTodos(@PathVariable String username){
-        return todoRepository.findTodosByUsername(username);
+    public ResponseEntity<List<Todo>> getTodos(@PathVariable String username){
+        return new ResponseEntity<>(todoRepository.findTodosByUsername(username), HttpStatus.OK);
     }
     @DeleteMapping("todo/{id}")
     public void deleteTodos(@PathVariable int id){
         todoRepository.deleteById(id);
     }
     @GetMapping("users/{username}/todo/{id}")
-    public Todo getTodo(@PathVariable String username, @PathVariable int id){
+    public ResponseEntity<Todo> getTodo(@PathVariable String username, @PathVariable int id){
         Todo todo = todoRepository.findTodosByUsernameAndId(username, id);
         if(todo == null )throw new TodoNotFoundException("Todo not found with id: "+id);
-        return todo;
+        return new ResponseEntity<>(todo, HttpStatus.OK);
     }
     @PutMapping("users/{username}/todo/{id}")
-    public Todo updateTodo(@PathVariable String username, @PathVariable int id, @RequestBody Todo todo){
+    public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable int id, @RequestBody Todo todo){
            //there is no update method in jpa repository. the save method does the update and create
-           todoRepository.save(todo);
-           return todo;
+           Todo updatedTodo = todoRepository.save(todo);
+           return new ResponseEntity<>(updatedTodo, HttpStatus.OK);
     }
     @PostMapping("users/{username}/todo")
-    public Todo addTodo(@PathVariable String username, @RequestBody Todo todo){
+    public ResponseEntity<Todo> addTodo(@PathVariable String username, @RequestBody Todo todo){
         //there is no update method in jpa repository. the save method does the update and create
-        return todoRepository.save(todo);
+        return new ResponseEntity<>(todoRepository.save(todo), HttpStatus.OK);
     }
 }
